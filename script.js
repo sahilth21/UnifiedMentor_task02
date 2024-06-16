b249b15a7c1748834836c67985b55de8fe629289
sahilth21 <sahilthawait21@gmail.com>
2024-06-16 23:31:34 +0530

    Add files via upload
    Image slider

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("carousel-images")[0].getElementsByTagName("img");
+  if (n >= slides.length) { slideIndex = 0 }
+  if (n < 0) { slideIndex = slides.length - 1 }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = "translateX(-" + slideIndex * 100 + "%)";
+  }
+}
